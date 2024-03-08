e606c3de48e337f3a8149a5f752f996d4e19fc19
jrodez <jeremie.rodez@irit.fr>
2024-03-08 13:46:12 +0100

    add a time progress log

--- src/ezpylog/__init__.py
+++ src/ezpylog/__init__.py
@@ -8,13 +8,15 @@ import enum
 from datetime import datetime
 from abc import ABC
 import logging
+import threading
+import time
 
 OLD_PYTHON = True if int(python_version().split(".")[1]) < 8 else False
 # OLD_PYTHON = True
 
-
-ONE_LINE_CHAR    = " "
-MULTI_LINE_CHAR  = "| "
+RET = "\r\x1b[K"
+ONE_LINE_CHAR = " "
+MULTI_LINE_CHAR = "| "
 
 if hasattr(sys, "_getframe"):
 
@@ -25,9 +27,9 @@ if hasattr(sys, "_getframe"):
 _srcfile = os.path.normcase(currentframe.__code__.co_filename)
 
 
-def get_caller_name():
+def get_caller_name(deepness=1):
     stack = inspect.stack()
-    fn = stack[1].filename
+    fn = stack[deepness].filename
     fn = fn.split("/")
     if len(fn) <= 1:
         fn = fn[-1].split("\\")
@@ -171,18 +173,25 @@ class EzpylogColoredFormatter(logging.Formatter):
             },
         }
 
-        if "compact" in kwargs and kwargs['compact']:
-            self.FORMAT = f"%(lvlprefix)s[%(levelname)5s]%(lvlsuffix)s {colors.CONTEXT}[%(funcName)-12s]{colors.ENDC} %(msgprefix)s%(message)s%(msgsuffix)s"
+        if "ezlogger" in kwargs:
+            self.ezlogger = kwargs["ezlogger"]
+        else:
+            self.ezlogger = None
+        if "compact" in kwargs and kwargs["compact"]:
+            self.compact = True
             self.LOG_LEVEL_COLOR["WARNING"]["label"] = "WARN"
             self.LOG_LEVEL_COLOR["CRITICAL"]["label"] = "FATAL"
+            self.FORMAT = f"%(lvlprefix)s[%(levelname)5s]%(lvlsuffix)s {colors.CONTEXT}[%(funcName)-12s]{colors.ENDC} %(msgprefix)s%(message)s%(msgsuffix)s"
         else:
+            self.compact = False
             self.FORMAT = f"%(asctime)s | %(lvlprefix)s[%(levelname)8s]%(lvlsuffix)s {colors.CONTEXT}[%(module)12s.%(funcName)-16s]{colors.ENDC} %(msgprefix)s%(message)s%(msgsuffix)s"
 
-    # FORMAT = f"%(asctime)s | %(lvlprefix)s[%(levelname)s]%(lvlsuffix)s {bcolors.CONTEXT}[%(module)s.%(funcName)s]{bcolors.ENDC} %(msgprefix)s%(message)s%(msgsuffix)s"
 
     def format(self, record):
         """Format log records with a default lvlprefix and lvlsuffix to terminal color codes that corresponds to the log level name."""
 
+        log_format = RET + self.FORMAT if self.ezlogger._progress_ctx is not None else self.FORMAT
+        
         if not hasattr(record, "lvlprefix"):
             record.lvlprefix = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get(
                 "lvlprefix"
@@ -200,7 +209,9 @@ class EzpylogColoredFormatter(logging.Formatter):
                 record.msgprefix = f"{colors.CHARACTER}{ONE_LINE_CHAR}{colors.ENDC}"
                 record.msg = record.msg[len(ONE_LINE_CHAR) :]
                 if record.msg.startswith(MULTI_LINE_CHAR):
-                    record.msgprefix += f"{colors.CHARACTER}{MULTI_LINE_CHAR}{colors.ENDC}"
+                    record.msgprefix += (
+                        f"{colors.CHARACTER}{MULTI_LINE_CHAR}{colors.ENDC}"
+                    )
                     record.msg = record.msg[len(MULTI_LINE_CHAR) :]
             record.msgprefix += self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get(
                 "msgprefix"
@@ -211,15 +222,17 @@ class EzpylogColoredFormatter(logging.Formatter):
                 "msgsuffix"
             )
 
-        if not hasattr(record, "module"):
+        if not hasattr(record, "module") or record.module is None:
             record.module = get_caller_name()[0]
-        if not hasattr(record, "funcName"):
+        if not hasattr(record, "funcName") or record.funcName is None:
             record.funcName = get_caller_name()[1]
+            # print(record.funcName,flush=True)
+            print(get_caller_name(deepness=4), flush=True)
 
         record.levelname = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get(
             "label"
         )
-        formatter = logging.Formatter(self.FORMAT, "%d-%m-%y %H:%M:%S")
+        formatter = logging.Formatter(log_format, "%d-%m-%y %H:%M:%S")
         return formatter.format(record)
 
 
@@ -243,6 +256,7 @@ class Logger(object):
         color_on_console: bool = True,
         stacklevel=2,
         compact=False,
+        to_stdout=False,
     ):
         """Initialize the logger.
         :param `min_level`: The minimum level of log to be displayed.
@@ -262,12 +276,13 @@ class Logger(object):
 
         self._handler = logging.Handler()
         self._handler.setFormatter(EzpylogFormatter())
-
-        self.stream_handler = logging.StreamHandler()
+        self._stream = sys.stderr if not to_stdout else sys.stdout
+        self.stream_handler = logging.StreamHandler(self._stream)
         self.stream_handler.setFormatter(
-            EzpylogColoredFormatter(compact=compact)
+            EzpylogColoredFormatter(compact=compact, ezlogger=self)
         ) if color_on_console else self.stream_handler.setFormatter(EzpylogFormatter())
         self._logger.addHandler(self.stream_handler)
+        self._handlers = [self._handler, self.stream_handler]
 
         if logfile is not None:
             self._file_handler = logging.FileHandler(
@@ -278,9 +293,11 @@ class Logger(object):
                 logfile_level if logfile_level is not None else min_level
             )
             self._logger.addHandler(self._file_handler)
+            self._handlers.append(self._file_handler)
 
         self._logger.setLevel(min_level)
         self.stacklevel = stacklevel
+        self._progress_ctx = None
 
     def set_level(self, level: int):
         """Set the minimum level of log to be displayed.
@@ -308,8 +325,8 @@ class Logger(object):
             text = msg
         splitted = text.split("\n")
         space = ONE_LINE_CHAR
-        space += MULTI_LINE_CHAR if len(splitted)>1 else "" 
-        for i,line in enumerate(splitted):
+        space += MULTI_LINE_CHAR if len(splitted) > 1 else ""
+        for i, line in enumerate(splitted):
             if OLD_PYTHON:
                 self._logger.log(level, f"{space}{line}")
             else:
@@ -319,23 +336,149 @@ class Logger(object):
                     stacklevel=self.stacklevel + stacklevel_offset,
                 )
 
+    def ttylog(
+        self,
+        msg,
+        level: int = logging.INFO,
+        stacklevel_offset=0,
+        flush=True,
+        end="\n",
+        begin="\r",
+        stack_infos=None,
+    ):
+        """Log a message to the tty only.
+        :param `msg`: The message to be logged.
+        :param `level`: The level of the message.
+        :param `context`: The context of the message.
+        """
+        if not isinstance(msg, SUPPORTED_TYPES):
+            try:
+                text = str(msg)
+            except:
+                raise TypeError(
+                    f"msg must be {SUPPORTED_TYPES.join(', ')} or convertable to a string"
+                )
+        elif isinstance(msg, dict):
+            text = dict_to_str(msg)
+        else:
+            text = msg
+        splitted = text.split("\n")
+        space = ONE_LINE_CHAR
+        space += MULTI_LINE_CHAR if len(splitted) > 1 else ""
+        for i, line in enumerate(splitted):
+            if OLD_PYTHON:
+                raise NotImplementedError("ttylog is not supported with python <3.8")
+            else:
+                # format the message with stream_handler formatter
+                formatter = self.stream_handler.formatter
+                record = logging.LogRecord(
+                    self._logger.name,
+                    level,
+                    self._logger.findCaller(),
+                    0,
+                    f"{space}{line}",
+                    None,
+                    None,
+                )
+                if stack_infos is None:
+                    file_name, line_number, func_name, sinfo = self._logger.findCaller(
+                        stacklevel=self.stacklevel + stacklevel_offset
+                    )
+                else:
+                    file_name, line_number, func_name, sinfo = stack_infos
+                record.module = file_name.split("/")[-1]
+                record.funcName = func_name
+                record.lineno = line_number
+                record.asctime = datetime.now().strftime("%d-%m-%y %H:%M:%S")
+                print(
+                    begin + formatter.format(record),
+                    end=end,
+                    flush=flush,
+                    file=self._stream,
+                )
+
+    def _log_continuous_progress(self, msg, step, loglevel, stacklevel_offset):
+        if self._progress_ctx is not None:
+            raise ValueError(
+                "A progress is already running. You need to call 'Logger.end_progress()' before starting a new one."
+            )
+
+        if OLD_PYTHON:
+            raise NotImplementedError("Progress is not supported with python <3.8")
+
+        stack_infos = self._logger.findCaller(
+            stacklevel=self.stacklevel + stacklevel_offset
+        )
+
+        def _progress():
+            timer = 0
+            while not self._progress_ctx[2]:
+                timer_str = f"{timer:.2f}"
+                self.ttylog(
+                    f"{msg} ({timer_str}s)",
+                    level=loglevel,
+                    stacklevel_offset=stacklevel_offset,
+                    flush=True,
+                    end="",
+                    stack_infos=stack_infos,
+                )
+                timer += step
+                time.sleep(step)
+
+        thread = threading.Thread(target=_progress)
+        thread.daemon = True
+        self._progress_ctx = (msg, loglevel, False, thread)
+        thread.start()
+        return self._progress_ctx
+
+    def _end_progress(self, msg, loglevel, stacklevel_offset, tty_only=False):
+        if OLD_PYTHON:
+            raise NotImplementedError("Progress is not supported with python <3.8")
+
+        self._stop_progress = True
+        self._progress_ctx = (
+            self._progress_ctx[0],
+            self._progress_ctx[1],
+            True,
+            self._progress_ctx[3],
+        )
+        self._progress_ctx[3].join()
+        if msg == "" or msg is None:
+            msg = self._progress_ctx[0]
+            msg = f"{msg} : Done." if not msg.endswith(":") else f"{msg} Done."
+        if not loglevel:
+            loglevel = self._progress_ctx[1]
+
+        if tty_only:
+            self.ttylog(msg, level=loglevel, stacklevel_offset=1)
+        else:
+            print("\r", end="", flush=True, file=self._stream)
+            self.log(msg, level=loglevel, stacklevel_offset=stacklevel_offset + 1)
+        self._progress_ctx = None
+
     def getLogger(self):
         return self._logger
 
-    def debug(self, msg=""):
-        self.log(msg, logging.DEBUG, stacklevel_offset=1)
+    def debug(self, msg="", stacklevel_offset=1):
+        self.log(msg, logging.DEBUG, stacklevel_offset)
 
-    def info(self, msg=""):
-        self.log(msg, logging.INFO, stacklevel_offset=1)
+    def info(self, msg="", stacklevel_offset=1):
+        self.log(msg, logging.INFO, stacklevel_offset)
 
-    def warning(self, msg=""):
-        self.log(msg, logging.WARNING, stacklevel_offset=1)
+    def warning(self, msg="", stacklevel_offset=1):
+        self.log(msg, logging.WARNING, stacklevel_offset)
 
-    def error(self, msg=""):
-        self.log(msg, logging.ERROR, stacklevel_offset=1)
+    def error(self, msg="", stacklevel_offset=1):
+        self.log(msg, logging.ERROR, stacklevel_offset)
 
-    def critical(self, msg=""):
-        self.log(msg, logging.CRITICAL, stacklevel_offset=1)
+    def critical(self, msg="", stacklevel_offset=1):
+        self.log(msg, logging.CRITICAL, stacklevel_offset)
+
+    def progress(self, msg="", loglevel=logging.INFO, step=0.09, stacklevel_offset=1):
+        self._log_continuous_progress(msg, step, loglevel, stacklevel_offset)
+
+    def end_progress(self, msg="", loglevel=logging.INFO, stacklevel_offset=1):
+        self._end_progress(msg, loglevel, stacklevel_offset)
 
     def close(self):
         for handler in self._logger.handlers:
@@ -351,7 +494,9 @@ def loggerdemo():
 
     a = 1234567
 
-    logger = Logger("TestLogger", min_level=logging.DEBUG, logfile="log.txt",compact=True)
+    logger = Logger(
+        "TestLogger", min_level=logging.DEBUG, logfile="log.txt", compact=True
+    )
     logger.log("Debug message\non\nmultiple\nline", logging.DEBUG)
     logger.log("Info message")
     logger.log("Warning message", logging.WARNING)
@@ -359,7 +504,11 @@ def loggerdemo():
     logger.log("Critical message", logging.CRITICAL)
     logger.warning({})
 
-    print("", flush=True)
+    logger.progress("Progress test", step=0.01)
+    time.sleep(2)
+    logger.log("Critical message", logging.CRITICAL)
+    logger.log("Warning message", logging.WARNING)
+    logger.end_progress()
 
     logger2 = Logger(
         __name__, logging.WARNING, color_on_console=False, logfile="log.txt"
